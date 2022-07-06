f2b6ebe1002d611c64cc262023f247b0654aba01
Dhananjay301996 <gadhavedhananjay123@gmail.com>
2022-07-06 19:19:28 +0530

    Added UC6 Program

--- src/day10_Assignment/main.java
+++ src/day10_Assignment/main.java
@@ -1,5 +1,8 @@
 package day10_Assignment;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class main {
 
@@ -49,6 +52,31 @@ public class main {
 	                i++;
 	            }
 	        }
+	        
 	        System.out.println(a1.listData);
-	}
-	}
+	        
+	       
+	        System.out.println("if you want to change any data of a person, reply y/n : ");
+	        check = addressbook.scan.nextLine();
+
+	        if (check.equals("y")) {
+
+	            System.out.println("enter name whose details you want to edit : ");
+	            String name = addressbook.scan.nextLine();
+
+	            for (contact cThrough : a1.listData) {
+
+	                if (cThrough.getFirstName().equals(name)) {
+	                    System.out.println("enter updated phone number : ");
+	                    cThrough.setPhone(addressbook.scan.next());
+	                    break;
+	                }
+	            }
+	        }
+
+	        Map<String, addressbook> map = new HashMap<>();
+	        map.put("1",a1);
+	        map.get(a1);
+	    }
+	   }
+	   	
